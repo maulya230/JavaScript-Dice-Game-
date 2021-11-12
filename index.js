b3f2b0ea07cbddeddad80e0213ed7caa24888cfc
maulya230 <75597705+maulya230@users.noreply.github.com>
2021-11-12 22:20:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
+//If player 2 wins
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
+//If Draw happens
 else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
